9a5feae8b5d27d5298a3d14e0e406c096881f646
Gray Karegeannes <11544089+sporaktu@users.noreply.github.com>
2020-08-24 01:30:24 -0400

    Adding new fonts

---
+++ components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home" :class="$style.home">
+    <h1>Gray and Katies</h1>
+    <h2>May 23rd, 2021</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style lang="scss" module>
+.home {
+  h1 {
+    color: blue;
+    font-family: 'Crimson Pro', serif;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        gray-and-katie-wedding-website
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+      <Home />
   </div>
 </template>
 
@@ -32,6 +9,8 @@ export default {}
 </script>
 
 <style>
+@import "./assets/design/index.scss";
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
