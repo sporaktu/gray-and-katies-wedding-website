80615c377daca3a3546300889c825d3915e2ec14
Gray Karegeannes <11544089+sporaktu@users.noreply.github.com>
2020-09-23 01:24:35 -0400

    Auth working (#1)
    * Making Auth Work
    
    * Adding back correct auth packages

---
+++ pages/guests.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="$style.guests">
+    <h1>Guests</h1>
+<!--    <vuetable-->
+<!--      :fields="Object.keys(guests[0])"-->
+<!--      :data="guests"-->
+<!--      :api-mode="false"-->
+<!--    />-->
+  </div>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2';
+
+export default {
+  name: "guests",
+  components: {Vuetable},
+  data() {
+    return {
+      guests: []
+    }
+  },
+  async asyncData({$axios}) {
+    return $axios.get('/guests').then(res => {
+      return {guests: res.data}
+    });
+  }
+}
+</script>
+
+<style lang="scss" module>
+  .guests {
+
+  }
+</style>
